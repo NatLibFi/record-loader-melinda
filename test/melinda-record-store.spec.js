5525f3711c7c08f27f321a12f1c2d1ebb25de536
natlibfi-arlehiko <natlibfi-arlehiko@users.noreply.github.com>
2016-10-11 13:27:42 +0300

    Add more reworked stuff

--- test/melinda-record-store.spec.js
+++ test/melinda-record-store.spec.js
@@ -35,14 +35,14 @@
       'chai/chai',
       'chai-as-promised',
       'es6-polyfills/lib/polyfills/promise',
-      '../lib/record-store/prototype'
+      '../lib/record-store/melinda'
     ], factory);
   } else if (typeof module === 'object' && module.exports) {
     module.exports = factory(
       require('chai'),
       require('chai-as-promised'),
       require('es6-polyfills/lib/polyfills/promise'),
-      require('../lib/record-store/prototype')
+      require('../lib/record-store/melinda')
     );
   }
 
